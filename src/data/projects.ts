448da93b12da3bedb95587be0b90b4ac7b9dc70e
gwendall <gwendall.esnault@gmail.com>
2024-02-16 00:02:35 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -152,7 +152,7 @@ const PROJECTS: Project[] = [
         name: 'What The Punk',
         url: 'https://twitter.com/WTP_Movie',
         logo: 'https://pbs.twimg.com/profile_images/1728116867191934976/OKnCNSFT_400x400.jpg',
-        description: 'Independant documentary on the amazing destiny of the Cryptopunks collection and the vivid Cryptoart scene Coproduced with ARTE & TokenAngels. Coming S1 24.',
+        description: 'Independant documentary on the amazing destiny of the Cryptopunks collection and the vivid Cryptoart scene.',
         categories: ['movie'],
         builders: []
     },
